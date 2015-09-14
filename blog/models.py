98a9da1dab54b87a8c2771c37a0c13f4b4a55b11
Archie <elbuckez@gmail.com>
2015-09-14 09:40:00 +0100

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-	tits = models.ForeignKey('auth.User')
+	user = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	created_date = models.DateTimeField(default=timezone.now)
-	Player_Name = models.DateTimeField(blank = True, null = True)
+	published_date = models.DateTimeField(blank = True, null = True)
 
 	def publish(self):
 		self.published_date = timezone.now()
